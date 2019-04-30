c7972be9c8e9cd6c6e5e5f709901f7c87602cf25
Mikael Holgersson <mikael@hogeran.se>
2019-04-30 13:38:06 +0200

    Skapat testfiler 190428 för publicering på server, har även skapat nya top och bot filer för header och footer

---
+++ index-190428.php
@@ -0,0 +1,23 @@
+<?php
+    
+	include("php/init.php");
+	
+	$bodyFront = true;
+	include("php/html_top_new.php");
+?>
+
+<div class="content-front">
+    <h1>Naturligt:  Vårt sortiment för kropp och själ</h1>
+    <h2>
+    Vårt material är speciellt utvecklat för att passa taktil massage.<br>
+    Allt är noga utprovat under många års användning. <br>
+    Endast det bästa är gott nog. Det är så du känner igen oss.  
+    </h2>
+    <img class="prod-front" src="media/images/produkter.png" />
+    <a href="http://www.youtube.com/user/TaktilUtbildning#p/a/u/1/fMx-kyNZQWQ" target="_blank" id="video">Film om hur vi arbetar<br />Exempel: Värmehund</a>
+</div><!-- .content-front -->
+
+
+<?php
+	include("php/html_bot_new.php");
+?>
---
+++ php/html_bot_new.php
@@ -0,0 +1,9 @@
+
+</div> <!--.content-front-->
+
+</div><!--StructureContentInner-->
+</div><!--StructureContent-->
+</div><!--content-wrap-->
+
+</body>
+</html>
