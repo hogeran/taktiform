6bad2bc27d570fad75088185501f3305d3e78985
Mikael Holgersson <mikael@hogeran.se>
2019-05-03 16:15:57 +0200

    sidan hudvård klar och ändrat transparens till .65

---
+++ pr_hudvard-190428.php
@@ -0,0 +1,56 @@
+<?
+	include("php/init.php");
+	
+	include("php/html_top_new.php");
+?>
+
+<div class="HudImages">
+
+<h1>
+Hudv&aring;rd, Oljor och Startpaket
+</h1>
+
+
+<p>
+Vårt sortiment är utvecklat för att användas i livets olika skeeenden, både baby och äldre, för frisk och hud med småsår. Används vid sol, bad, dusch.Perfekt att ta bort makeup med. Stimulerar blodcirkulation och sårläkning och är helt fantastisk att ge massage med. Gjord på exklusiva vegetabiliska och ekologiskt odlade produkter:
+</p>
+<p>
+JOJOBAOLJA är lyxig, sammetslen, mjukgörande och fuktighetsbevarande.<br>
+SHEASMÖR är mild, bra vid känslig hud, är som balsam för torr, narig sprucken hud.<br>
+SOLROSOLJAN är en ”torr” olja Absorberas lätt. Är skyddande och näringsrik.<br>
+RAPSOLJA är mjukgörande och näringsrik. Mycket bra vid kåda och irritationer.<br>
+MANDARIN/CITRON, frisk och uppiggande. Finns i MORGON.<br>
+LAVENDEL bra vid klåda och irriterad hud. Finns i LAVENDEL.<br>
+ROSMARIN värmande och lindrande Finns i ULLSALVA.<br>
+Alla oljor har hög halt av E-vitamin som antioxidant. Stimulerar blodcirkulation och sårläkning.
+</p>
+
+<? ShowProduct(3017, 'Right'); ?>
+<? ShowProduct(3010, 'Half'); ?>
+<? ShowProduct(3011, 'Half'); ?>
+<? ShowProduct(3012, 'Full'); ?>
+<? ShowProduct(3021, 'Full'); ?>
+<? ShowProduct(3023, 'Full'); ?>
+
+<? ShowProduct(3083, 'Full'); ?>
+<? ShowProduct(3082, 'Full'); ?>
+
+<? ShowProductClearing(); ?>
+
+
+<!--<p>
+	Vår ansiktsolja har vi utvecklat för att tillgodose behovet av en extra mjuk olja som passar ansiktets känsliga hud. Ansiktsoljans egenskaper är att ge skydd och näring. Ger också en gyllene lyster. 35 ml.
+</p>
+-->
+
+
+
+<!--<p>
+	Fåren är tåliga och ger oss en fantastisk naturlig produkt. Den är basen i vår unika ullsalva som är återfuktande och mjukgörande. För torra läppar, nagelband och självsprickor, samt till både små och stora som använder blöja. Används också vid psoriasis, skav- och småsår. Är huden fuktig utvecklas lanolin.  50 ml.
+</p>-->
+<? //ShowProduct(3023, 'Full'); ?>
+<? //ShowProductClearing(); ?>
+</div><!-- .HudImages -->
+<?
+	include("php/html_bot_new.php");
+?>
