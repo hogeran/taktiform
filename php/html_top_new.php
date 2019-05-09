67e76c75ca5ab4cfbc9c4c75fffd1d4d3c801987
Mikael Holgersson <mikael@hogeran.se>
2019-05-09 09:28:12 +0200

    Top-raden mobilanpassad Lagt till viewport funktion  Changes to be committed: 	modified:   Index-190428.html (testfil) 	modified:   media/styles-190428.css 	modified:   php/html_top_new.php

--- php/html_top_new.php
+++ php/html_top_new.php
@@ -5,6 +5,7 @@
 <title>Taktiform</title>
 <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="media/styles-190428.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script type="text/javascript" src="jQuery.js"></script>
 <script type="text/javascript">
 
